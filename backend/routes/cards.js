0549e5a821033c86c6883f4964eb18707769a66f
lsmith <therealshaunsred@gmail.com>
2023-09-26 09:36:08 -0400

    Created routed for deck and card resources

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -4,12 +4,13 @@
 
 const express = require('express')
 const router = express.Router()
+const cardCtrl = require('../controllers/cards')
 
 ///////////////////////////////
 // ROUTES
 ////////////////////////////////
 
 //Create
-
+router.post("/", cardCtrl.create);
 
 module.exports = router
--- backend/routes/decks.js
+++ backend/routes/decks.js
@@ -4,20 +4,25 @@
 
 const express = require('express')
 const router = express.Router()
+const deckCtrl = require('../controllers/decks')
 
 ///////////////////////////////
 // ROUTES
 ////////////////////////////////
 
 //Index
-router.get("/", )
+router.get("/", deckCtrl.index)
 
 //Show
+router.get("/:id", deckCtrl.show)
 
 //Create
+router.post("/", deckCtrl.create);
 
 //Delete
+router.delete("/:id", deckCtrl.destroy);
 
 //Update
+router.put("/:id", deckCtrl.update);
 
 module.exports = router
