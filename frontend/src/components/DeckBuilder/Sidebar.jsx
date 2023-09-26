004fccfb0417ca10d9e7832d137ca63a36e2b6d8
lsmith <therealshaunsred@gmail.com>
2023-09-26 17:27:46 -0400

    Card search added

--- frontend/src/components/DeckBuilder/Sidebar.jsx
+++ frontend/src/components/DeckBuilder/Sidebar.jsx
@@ -1,45 +1,64 @@
-import { Box, Button, Drawer } from "@mui/material";
+import { Box, Button, Drawer, Stack } from "@mui/material";
 import { useState } from "react";
+import { searchCard } from "../../utilities/scryfall-service";
 
-export default function Sidebar({state, setState}){
+export default function Sidebar({ state, setState }) {
+  const [search, setSearch] = useState("");
+  const [searchResults, setResults] = useState(null);
 
-    const [search, setSearch] = useState('')
-    const [searchResults, setResults] = useState([])
+  function toggleOff() {
+    setState(false);
+  }
 
-    function toggleOff(){
-        setState(false)
+  function handleChange(event) {
+    setSearch(event.target.value);
+  }
+
+  async function handleSearch() {
+    setSearch("");
+    try {
+      const foundCard = await searchCard(search);
+      setResults(foundCard);
+    } catch (error) {
+      console.log(error);
+      setResults("error");
     }
+  }
 
-    function handleChange(event) {
-        setSearch(event.target.value);
+  function searched() {
+    if (searchResults === "error") {
+      return <p>No card found. Change your search and try again.</p>;
+    } else if (searchResults) {
+      return (
+        <img
+          src={searchResults.image_uris.normal}
+          width="300px"
+          alt={searchResults.name}
+        />
+      );
+    } else {
+      return (
+        <p>
+          Enter your search above and press the search button to find a card
+        </p>
+      );
     }
+  }
 
-    function handleSearch(){
-        
-    }
-
-    return(
-        <Drawer 
-        anchor='right'
-        open={state}
-        onClose={toggleOff}
-        >
-            <Box 
-                sx={{
-                    width: "50rem",
-                }}
-            >
-                <input 
-                type="text"
-                value={search}
-                onChange={handleChange}
-                ></input>
-                <Button
-                    onClick={handleSearch}
-                >
-                    Search
-                </Button>
-            </Box>
-        </Drawer>
-    )
+  return (
+    <Drawer anchor="right" open={state} onClose={toggleOff}>
+      <Box
+        sx={{
+          width: "50rem",
+        }}
+        className="sidebar"
+      >
+        <Stack>
+          <input type="text" value={search} onChange={handleChange}></input>
+          <Button onClick={handleSearch}>Search</Button>
+        </Stack>
+        {searched()}
+      </Box>
+    </Drawer>
+  );
 }
--- frontend/src/utilities/scryfall-service.js
+++ frontend/src/utilities/scryfall-service.js
@@ -2,7 +2,7 @@ import * as scryApi from "./scryfall-api"
 
 export async function searchCard(query) {
     try {
-      const foundDeck = await deckAPI.search(query);
+      const foundDeck = await scryApi.search(query);
       return foundDeck;
     } catch (err) {
       throw err;
