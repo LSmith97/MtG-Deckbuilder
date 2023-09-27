176e5c6f25b2c5b4d916ab9fc270332d428b9f36
lsmith <therealshaunsred@gmail.com>
2023-09-27 15:50:23 -0400

    Updated Header styling

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,11 +1,16 @@
 import Nav from "./Nav";
-import './Header.css'
+import "./Header.css";
+import { Stack } from "@mui/material";
 
-export default function Header(){
-    return(
-        <header>
-            <h1>MtG Deck Builder</h1>
-            <Nav/>
-        </header>
-    )
+export default function Header() {
+  return (
+    <header  >
+      <Stack padding={2} spacing={2} direction="row" alignItems="center" justifyContent="center">
+        <img src="https://i.imgur.com/pjg3O8F.png" alt="logo" width="50px" id="logo"/>
+        <h1>MtG Deck Builder</h1>
+      </Stack>
+
+      <Nav />
+    </header>
+  );
 }
