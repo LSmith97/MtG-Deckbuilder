48aa489d27316f1cb0a20dd11e5903d526b87be9
lsmith <therealshaunsred@gmail.com>
2023-09-28 13:36:16 -0400

    Added profile page route

--- frontend/src/components/Main/index.js
+++ frontend/src/components/Main/index.js
@@ -3,18 +3,20 @@ import About from "../../pages/About";
 import Decks from "../../pages/Decks/";
 import Show from "../../pages/Show/";
 import New from "../../pages/New";
-import Edit from "../../pages/Edit"
+import Edit from "../../pages/Edit";
+import Profile from "../../pages/Profile";
 
-export default function Main(){
-    return(
-        <main>
-            <Routes>
-                <Route path="/" element={<About />} />
-                <Route path="/decks" element={<Decks />} />
-                <Route path="/decks/new" element={<New />} />
-                <Route path="/decks/:id" element={<Show />} />
-                <Route path="/decks/:id/edit" element={<Edit/>}/>
-            </Routes>
-        </main>
-    )
+export default function Main() {
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/decks" element={<Decks />} />
+        <Route path="/decks/new" element={<New />} />
+        <Route path="/decks/:id" element={<Show />} />
+        <Route path="/decks/:id/edit" element={<Edit />} />
+        <Route path="/users/:id" element={<Profile />} />
+      </Routes>
+    </main>
+  );
 }
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,29 +1,6 @@
 export default function Profile(){
 
 return (
-  <>
-    <h2>Deck Details</h2>
-    <Paper className="show-stats" elevation={6}>
-      <Stack>
-        <h3>Deck Name: {deck.name}</h3>
-        <Stack
-          spacing={1}
-          direction="row"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <h4>Created By:</h4>
-          <Avatar
-            alt={deck.owner.nickname}
-            src={deck.owner.picture}
-            sx={{ height: 30, width: 30 }}
-          />
-          <h4>{deck.owner.nickname}</h4>
-        </Stack>
-        <h4>Total Cards: {cardNo} </h4>
-        <h4>Unique Cards: {deck.cardList.length}</h4>
-      </Stack>
-    </Paper>
-  </>
+  <h2>Profile</h2>
 )
 }
